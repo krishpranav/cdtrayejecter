8aaee1719642a938d88a3d091f6797e4f1069467
krishpranav <krisna.pranav@gmail.com>
2020-10-08 08:36:08 +0530

    done changes

--- control.cpp
+++ control.cpp
@@ -1,14 +1,14 @@
+//a simple c++ tool for ejecting cd tray on a laptop or pc
+
 //imports
-//a simple c++ tool for ejecting cd tray
 #include <Windows.h>
 #include <stdio.h>
 
 using namespace std;
 
-int main()
-{
-    mciSendString("set cd audio door open", NULL, NULL, NULL);
-    printf("Cd Tray Opened SuccessFully")
+int main(){
+    mciSendString("set cd audio tray open", NULL, NULL, NULL)
+    printf("cd tray opened successfully");
     getchar();
     return EXIT_SUCESS;
 }
