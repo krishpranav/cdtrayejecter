d8d14bf332d427ce4e43208a729bb4282e12245f
krishpranav <krisna.pranav@gmail.com>
2020-10-09 08:27:52 +0530

    initial commit

--- cd-tray-eject.cpp
+++ cd-tray-eject.cpp
@@ -2,16 +2,3 @@
 //tool author: krishpranav
 //use this tool for legal purpose
 //tool name: cdtrayejecter
-
-//imports
-#include <stdio.h>
-#include <Windows.h>
-
-using namespace std;
-
-int main(){
-    msiSendString("set cdaudio tray open", NULL, NULL, NULL);
-    printf("cd tray opened successfully")
-    getchar();
-    return EXIT_SUCCESS;
-}
